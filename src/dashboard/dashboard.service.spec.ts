12a496748b9eaa24ffe8247e7dbdeee0a2d6f035
AlanHedz <alan.herjuarez97@gmail.com>
2025-07-27 19:08:49 -0600

    added prisma and remove controller spec

--- src/dashboard/dashboard.service.spec.ts
+++ src/dashboard/dashboard.service.spec.ts
@@ -1,15 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DashboardService } from './dashboard.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+const prismaMock = {
+  income: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn()
+  }
+}
 
 describe('DashboardService', () => {
   let service: DashboardService;
+  let prisma: typeof prismaMock
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DashboardService],
+      providers: [
+        DashboardService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock
+        }
+      ],
     }).compile();
 
     service = module.get<DashboardService>(DashboardService);
+    prisma = module.get(PrismaService)
   });
 
   it('should be defined', () => {
